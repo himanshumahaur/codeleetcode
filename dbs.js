e40633ffffeccb05f0834f89bb4d1ec78f83a968
Himanshu Mahaur <76582074+himanshumahaur@users.noreply.github.com>
2025-06-20 23:29:42 +0530

    Update dbs.js

--- dbs.js
+++ dbs.js
@@ -752,8 +752,21 @@ public:
     }
 };`
     },
-    0: {
-        problem: ``,
-        code:  ``
+    1752: {
+        problem: `Check if Array Is Sorted and Rotated`,
+        code:  `class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        for(int i=0; i<nums.size()-1; i++) {
+            if(nums[i] > nums[i+1]) {
+                count++;
+                if(count > 1) return false; 
+            }
+        }
+        if(count==1 && nums[0] < nums[nums.size()-1]) return false;
+        return true;
+    }
+};`
     }
 }
